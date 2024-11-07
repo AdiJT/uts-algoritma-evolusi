62c07fff009c381a1b77add40c3cb171df9fa211
AdiJT <aditaklal@gmail.com>
2024-11-07 08:39:38 +0800

    Sedikit

--- UTSAlgoEvolusi.Core/Seleksi/TournamentSelection.cs
+++ UTSAlgoEvolusi.Core/Seleksi/TournamentSelection.cs
@@ -17,12 +17,14 @@ public class TournamentSelection<TAlel, TAsli> : ISeleksi<TAlel, TAsli>
         var hasilEvaluasi = populasi.Select(k => fungsiObjektif(encoding.Decode(k))).ToList();
         var populasiBaru = new List<Kromoson<TAlel>>();
         var random = new Random();
-        Func<double, double, bool> compareFunc = jenis == JenisAgen.Max ? (double f1, double f2) => f1 >= f2 : (double f1, double f2) => f1 <= f2;
+        Func<double, double, bool> compareFunc = jenis == JenisAgen.Max ? 
+            (double f1, double f2) => f1 >= f2 : 
+            (double f1, double f2) => f1 <= f2;
 
         for(int i = 0; i <  populasi.Count; i++)
         {
             var kromoson1Index = random.Next(0, populasi.Count);
-            var kromoson2Index = random.Next(0, populasi.Count, kromoson1Index);
+            var kromoson2Index = random.Next(0, populasi.Count, exclude: kromoson1Index);
 
             var fitness1 = hasilEvaluasi[kromoson1Index];
             var fitness2 = hasilEvaluasi[kromoson2Index];
